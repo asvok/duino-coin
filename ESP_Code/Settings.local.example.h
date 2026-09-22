cc19a814d85c9685add6f83884f476480bdda130
Asvok <rachmat.ardhanic3@gmail.com>
2026-09-22 12:55:28 +0700

    Keep Duino-Coin device settings local

---
+++ ESP_Code/Settings.local.example.h
@@ -0,0 +1,12 @@
+// Copy this file to Settings.local.h and enter your private settings there.
+// Settings.local.h is ignored by Git.
+#ifndef DUCO_SETTINGS_LOCAL_H
+#define DUCO_SETTINGS_LOCAL_H
+
+extern char *DUCO_USER = "your_username";
+extern char *MINER_KEY = "";
+extern char *RIG_IDENTIFIER = "Auto";
+extern const char SSID[] = "your_wifi_name";
+extern const char PASSWORD[] = "your_wifi_password";
+
+#endif
